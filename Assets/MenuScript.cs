5d03503085fceb1526fffea028e91638a9aa3616
Logan3106 <529295@chrome.escg.ac.uk>
2023-02-10 16:01:13 +0000

    update1

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitButton()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
